2fdf43758115c7cbe3ef7ef2f0430309b7a45353
earlstdmg <151489856+earlstdmg@users.noreply.github.com>
2024-01-11 15:06:43 +0800

    Add files via upload

---
+++ escript.js
@@ -0,0 +1,8 @@
+const c = document.getElementById("myCanvas");
+        const ctx = c.getContext("2d");
+        
+         
+        ctx.beginPath(); 
+        ctx.moveTo(0,0); 
+        ctx.lineTo(300,100); 
+        ctx.stroke();
